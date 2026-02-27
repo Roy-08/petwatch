32e0702c9afd6aa6fe19a0be160d28c7f95118a1
ROHIT_YERUNKAR_08 <rohityerunkar1508@gmail.com>
2026-02-27 15:33:04 +0530

    Update mongodb.ts

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/pet-adoption";
+const MONGODB_URI = process.env.MONGODB_URI ;
 
 interface MongooseCache {
   conn: typeof mongoose | null;
@@ -39,5 +39,6 @@ async function connectDB() {
 
   return cached.conn;
 }
+
 
 export default connectDB;
